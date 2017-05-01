96aba31c2cd5afeae5dbbd179ee3e30448f616a2
maxsvetlik <maxsvetlik@gmail.com>
2017-05-01 13:17:39 -0500

    some small changes to joy. added BSD licenses

--- svenzva_drivers/src/svenzva_drivers/revel_gripper_server.py
+++ svenzva_drivers/src/svenzva_drivers/revel_gripper_server.py
@@ -1,4 +1,36 @@
 #!/usr/bin/env python
+# Software License Agreement (BSD License)
+#
+# Copyright (c) 2017 Svenzva Robotics
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+#  * Neither the name of University of Arizona nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
 """
 Svenzva's Revel Gripper action interface
 
@@ -69,8 +101,7 @@ class RevelGripperActionServer():
                 success = False
                 return
         self._result.success = True
-        self._result.sequence = self._feedback.sequence
-        self._as.set_succeeded(self._result)
+        #self._as.set_succeeded(self._result)
 
     def motor_state_cb(self, data):
         self.motor_state = data.motor_states[self.motor_id - 1]
@@ -95,7 +126,6 @@ class RevelGripperActionServer():
         #stop movement
         self.mx_io.set_torque_goal(self.motor_id, 0)
 
-        rospy.spin()
 
     def open_gripper(self, force):
         cur_pos = 0 #self.motor_state.position
