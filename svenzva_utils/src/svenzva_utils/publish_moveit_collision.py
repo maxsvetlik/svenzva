36e0839f567e8e304e77898e4afc373b83549fc6
maxsvetlik <maxsvetlik@gmail.com>
2018-03-18 11:06:33 -0500

    added wall collisions

--- svenzva_utils/src/svenzva_utils/publish_moveit_collision.py
+++ svenzva_utils/src/svenzva_utils/publish_moveit_collision.py
@@ -24,28 +24,41 @@ def publish_scene_collision():
   #Create buffer shapes
   buff1 = shape_msgs.msg.SolidPrimitive()
   buff1.type = buff1.BOX
-  buff1.dimensions = [0.6, 1.4, 0.05]
+  buff1.dimensions = [0.6, 1.6, 0.07]
   buff1_pose = geometry_msgs.msg.Pose()
-  buff1_pose.position.x = -0.4
+  buff1_pose.position.x = -0.5
+  buff1_pose.position.z = 0.03
 
   buff2_pose = geometry_msgs.msg.Pose()
-  buff2_pose.position.x = 0.4
+  buff2_pose.position.x = 0.5
+  buff2_pose.position.z = 0.03
 
   buff3 = shape_msgs.msg.SolidPrimitive()
   buff3.type = buff3.BOX
-  buff3.dimensions = [1.4, 0.6, 0.05]
+  buff3.dimensions = [1.6, 0.6, 0.07]
   buff3_pose = geometry_msgs.msg.Pose()
-  buff3_pose.position.y = -0.4
+  buff3_pose.position.y = -0.5
+  buff3_pose.position.z = 0.03
 
   buff4_pose = geometry_msgs.msg.Pose()
-  buff4_pose.position.y = 0.4
+  buff4_pose.position.y = 0.5
+  buff4_pose.position.z = 0.03
+
+
+  #Example of wall, behind robot
+  wall1 = shape_msgs.msg.SolidPrimitive()
+  wall1.type = wall1.BOX
+  wall1.dimensions = [2.5, 0.1, 1.0]
+  wall1_pose = geometry_msgs.msg.Pose()
+  wall1_pose.position.y = 0.3
+  wall1_pose.position.z = 0.5
 
   #Create a ground plane
   coll_ob.operation = coll_ob.ADD
   coll_ob.header.frame_id = "base_link"
   coll_ob.id = "ground_plane"
-  coll_ob.primitives.append(shape)
-  coll_ob.primitive_poses.append(g_pose)
+  #coll_ob.primitives.append(shape)
+  #coll_ob.primitive_poses.append(g_pose)
 
   coll_ob.primitives.append(buff1)
   coll_ob.primitive_poses.append(buff1_pose)
@@ -59,6 +72,9 @@ def publish_scene_collision():
   coll_ob.primitives.append(buff3)
   coll_ob.primitive_poses.append(buff4_pose)
 
+
+  coll_ob.primitives.append(wall1)
+  coll_ob.primitive_poses.append(wall1_pose)
 
   #Annoyingly need to publish many times, if not spinning continuously
   collision_pub.publish(coll_ob)
