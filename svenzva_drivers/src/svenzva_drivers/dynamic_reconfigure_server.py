3f96853a0add866da0a61d907486c1a9e3257c6e
maxsvetlik <maxsvetlik@gmail.com>
2018-11-18 12:49:38 -0600

    further pre-integration changes

--- svenzva_drivers/src/svenzva_drivers/dynamic_reconfigure_server.py
+++ svenzva_drivers/src/svenzva_drivers/dynamic_reconfigure_server.py
@@ -7,36 +7,62 @@ from dynamic_reconfigure.server import Server
 from svenzva_drivers.cfg import RevelFirmwareDynamicConfig
 
 
-def callback(config, level):
-    global is_first_configuration, last_configuration
-    #pp = pprint.PrettyPrinter(indent=4)
-    #pp.pprint(config.groups.groups)
-    #config.groups.groups.acceleration.state = False
+class RevelDynamicParameterServer():
 
-    if is_first_configuration:
-        config.groups.groups.acceleration.state = False
-        config.groups.groups.velocity.state = False
-        config.groups.groups.pid.state = False
-        last_configuration = config.copy()
-        is_first_configuration = False
+    def __init__(self, controller_namespace, mx_io):
+        self.is_first_configuration = True
+        self.last_configuration = {}
+
+    def callback(self, config, level):
+        #first run
+        if self.is_first_configuration:
+            self.last_configuration = config.copy()
+            self.is_first_configuration = False
+            return config
+
+        #if user hasn't enabled parameter control, do not process parameter changes
+        if not config.enable_parameter_control:
+            return self.last_configuration
+
+        #process parameter changes
+        if level & 1:
+            print "Changed acceleration profile!"
+            print level
+        if level & 1<<1:
+            print "Changed velocity profile!"
+            print level
+        if level & 1<<2:
+            print "Changed Joint 1 PID!"
+            print level
+        if level & 1<<3:
+            print "Changed Joint 2 PID!"
+            print level
+        if level & 1<<4:
+            print "Changed Joint 3 PID!"
+            print level
+        if level & 1<<5:
+            print "Changed Joint 4 PID!"
+            print level
+        if level & 1<<6:
+            print "Changed Joint 5 PID!"
+            print level
+        if level & 1<<7:
+            print "Changed Joint 6 PID!"
+            print level
+        if level & 1<<8:
+            print "Changed Joint 7 PID!"
+            print level
         return config
 
-    if not config.enable_parameter_control:
-        return last_configuration
+    #def set_position_p_gain(self, servo_id, p_gain):
+    #def set_position_PID(self, ):
+        #mx_io.set_position_p_gain(self, servo_id, p_gain):
 
-    for g in config.groups.groups:
-        if config.groups.groups[g].state == False:
-            config.groups.groups[g] = last_configuration.groups.groups[g]
-
-    return config
+    def start(self):
+        srv = Server(RevelFirmwareDynamicConfig, self.callback)
+        rospy.spin()
 
 if __name__ == "__main__":
-    global is_first_configuration, last_configuration
-    is_first_configuration = True
-    last_configuration = {}
-
-    rospy.init_node("svenzva_drivers_reconfigure", anonymous = False)
-
-    srv = Server(RevelFirmwareDynamicConfig, callback)
-
-    rospy.spin()
+        rospy.init_node("svenzva_drivers_reconfigure", anonymous = False)
+        rdps = RevelDynamicParameterServer('', '')
+        rdps.start()
