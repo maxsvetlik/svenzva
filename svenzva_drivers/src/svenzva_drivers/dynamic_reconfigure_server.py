d4606068d81c337b4cc20b36ceb0fe3308a61376
maxsvetlik <maxsvetlik@gmail.com>
2018-11-18 12:04:25 -0600

    further changes. cfg more or less done. pre-integration

--- svenzva_drivers/src/svenzva_drivers/dynamic_reconfigure_server.py
+++ svenzva_drivers/src/svenzva_drivers/dynamic_reconfigure_server.py
@@ -1,17 +1,42 @@
 #!/usr/bin/env python
 
 import rospy
+import pprint
 
 from dynamic_reconfigure.server import Server
 from svenzva_drivers.cfg import RevelFirmwareDynamicConfig
 
+
 def callback(config, level):
-    rospy.loginfo("""Reconfigure Request: {int_param}, {double_param},\
-          {str_param}, {bool_param}, {size}""".format(**config))
+    global is_first_configuration, last_configuration
+    #pp = pprint.PrettyPrinter(indent=4)
+    #pp.pprint(config.groups.groups)
+    #config.groups.groups.acceleration.state = False
+
+    if is_first_configuration:
+        config.groups.groups.acceleration.state = False
+        config.groups.groups.velocity.state = False
+        config.groups.groups.pid.state = False
+        last_configuration = config.copy()
+        is_first_configuration = False
+        return config
+
+    if not config.enable_parameter_control:
+        return last_configuration
+
+    for g in config.groups.groups:
+        if config.groups.groups[g].state == False:
+            config.groups.groups[g] = last_configuration.groups.groups[g]
+
     return config
 
 if __name__ == "__main__":
-    rospy.init_node("svenzva_drivers", anonymous = True)
+    global is_first_configuration, last_configuration
+    is_first_configuration = True
+    last_configuration = {}
+
+    rospy.init_node("svenzva_drivers_reconfigure", anonymous = False)
 
     srv = Server(RevelFirmwareDynamicConfig, callback)
+
     rospy.spin()
